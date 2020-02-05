fb2c6d382e5aa2e5285f425d494a8c9123e0bff4
BlazingArr0WS <dakota@area-investments.com>
2020-02-05 15:12:22 -0800

    Updates

---
+++ precache-manifest.60865000019793b30ef79bd64c6684aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a142f380328c793d2e192c3514a3216f",
+    "url": "/theEventHorse/index.html"
+  },
+  {
+    "revision": "6815d2c9bd08224b3e0d",
+    "url": "/theEventHorse/static/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "5bdac607dcbd41e44934",
+    "url": "/theEventHorse/static/css/main.cf1d025c.chunk.css"
+  },
+  {
+    "revision": "6815d2c9bd08224b3e0d",
+    "url": "/theEventHorse/static/js/2.8ef4a14c.chunk.js"
+  },
+  {
+    "revision": "a01efedc0251e965f76a46969bc547a3",
+    "url": "/theEventHorse/static/js/2.8ef4a14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdac607dcbd41e44934",
+    "url": "/theEventHorse/static/js/main.7ccc152f.chunk.js"
+  },
+  {
+    "revision": "dc6c162135f3716281d4",
+    "url": "/theEventHorse/static/js/runtime-main.dea543b0.js"
+  }
+]);
